34fd3b47a4fd4cbd5dbca293197dedbb7204042f
Mark Muchai <muchaimark6@gmail.com>
2023-07-19 19:44:16 +0300

    initial commit

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ function ArticleList({ posts }) {
   return (
     <main>
       {posts.map((post, index) => (
-        <Article key={index} title={post.title} date={post.date} preview={post.preview} />
+        <ArticleList key={index} title={post.title} date={post.date} preview={post.preview} />
       ))}
     </main>
   );
